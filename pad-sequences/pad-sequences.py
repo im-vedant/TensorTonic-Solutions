7cccbff2b4a0699227d055a4a6ab93da164b86c6
Vedant Gupta <115912707+im-vedant@users.noreply.github.com>
2026-07-24 23:12:04 +0530

    Solve pad-sequences

--- pad-sequences/pad-sequences.py
+++ pad-sequences/pad-sequences.py
@@ -4,7 +4,7 @@ def pad_sequences(seqs, pad_value=0, max_len=None):
     """
     Returns: np.ndarray of shape (N, L) where:
       N = len(seqs)
-      L = max_len if provided else max(len(seq) for seq in seqs) or 0
+      L =lkjfdlksj flds max_len if provided else max(len(seq) for seq in seqs) or 0
     """
     if not seqs:
         return np.array([], dtype=float).reshape(0, 0)
